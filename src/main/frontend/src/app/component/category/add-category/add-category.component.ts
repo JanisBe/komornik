9974074cddee002ddec12fd0fc175b17bfdb3fb4
Janis <janis666@gmail.com>
2023-06-13 15:14:43 +0200

    add-category.component.ts też edytuje

--- src/main/frontend/src/app/component/category/add-category/add-category.component.ts
+++ src/main/frontend/src/app/component/category/add-category/add-category.component.ts
@@ -4,6 +4,7 @@ import {CategoryService} from "../../../service/category.service";
 import {Category} from "../../../model/category";
 import {ActivatedRoute, Router} from "@angular/router";
 import {SnackbarService} from "../../../service/snackbar.service";
+import {Observer} from "rxjs";
 
 @Component({
   selector: 'add-category',
@@ -21,39 +22,61 @@ export class AddCategoryComponent implements OnInit {
               private route: ActivatedRoute) {
   }
 
+  resolve: Observer<Category> = {
+    next: (result) => {
+      this.snackbarService.displayMessage(`Nowa kategoria ${result.name} założona!`);
+      this.onCancel();
+    },
+    error: () => {
+      this.snackbarService.displayMessage(`Nie udało się założyć kategorii`);
+    },
+    complete: () => {
+    }
+  }
+
   ngOnInit() {
     if (!!this.route.snapshot.params['id']) {
       this.currentCategoryId = this.route.snapshot.params['id'];
       this.categoryService.findById(this.currentCategoryId).subscribe(
-        category => this.currentCategory = category
+        category => {
+          this.currentCategory = category;
+          this.form.get('name')?.patchValue(category.name);
+        }
       )
     }
     this.initForm();
   }
 
   onSubmit() {
+    if (!!this.currentCategory) {
+      this.patchCategory();
+    } else {
+      this.saveCategory();
+    }
+  }
+
+  private saveCategory() {
     const newCategory: Category = {
       name: this.form.value.name
     }
-    this.categoryService.createCategory(newCategory).subscribe({
-      next: (result) => {
-        this.snackbarService.displayMessage(`Nowa kategoria ${result.name} założona!`);
-        this.onCancel();
-      },
-      error: () => {
-        this.snackbarService.displayMessage(`Nie udało się założyć kategorii ${newCategory.name}`);
-      }
-    });
+    this.categoryService.createCategory(newCategory).subscribe(this.resolve);
   }
 
   onCancel() {
     this.router.navigate(['category/list']);
   }
 
+  private patchCategory() {
+    const categoryToSave: Category = {
+      name: this.form.value.name,
+      id: this.currentCategory.id
+    }
+    this.categoryService.editCategory(categoryToSave).subscribe(this.resolve);
+  }
+
   private initForm() {
-    const categoryName = this.currentCategory.name;
     this.form = new FormGroup({
-      name: new FormControl(categoryName, Validators.required)
+      name: new FormControl(null, Validators.required)
     });
   }
 }
