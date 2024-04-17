faf14e5fac4d72d84ee1b6057d0baa53af06dee2
Janis Blatsios <janis666@gmail.com>
2024-04-17 09:46:46 +0200

    WIP - jakoś działa

---
+++ src/main/java/pl/janis/komornik/filter/SpaWebFilter.java
@@ -0,0 +1,32 @@
+package pl.janis.komornik.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class SpaWebFilter extends OncePerRequestFilter {
+
+    /**
+     * Forwards any unmapped paths (except those containing a period) to the client {@code index.html}.
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+        String path = request.getRequestURI();
+        if (!path.startsWith("/api") &&
+                !path.startsWith("/login") &&
+                !path.startsWith("/auth") &&
+                !path.contains(".") &&
+                path.matches("/(.*)")) {
+            request.getRequestDispatcher("/index.html").forward(request, response);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
