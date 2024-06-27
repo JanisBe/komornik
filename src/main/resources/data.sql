ede77b17a0028418f47c0b21c79d0c62750e22ab
Janis Blatsios <janis666@gmail.com>
2024-06-27 13:39:41 +0200

    zmiana input number zmiana sqla zmiana mavena na 3.6.9 czy ile tam DEPLOY

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,19 +1,16 @@
-INSERT INTO komornik.users (id, avatar, mail, username, password, is_verified)
-VALUES (1, null, 'a@a.pl', 'a', '$2a$10$rgTJrdROc5a0MJiTmSU8qewdi6Ro2elGZf6Rmuc5/rl6nam3P3z.S', true),
-       (2, null, 'aa@aa.pl', 'aa', '$2a$10$rgTJrdROc5a0MJiTmSU8qewdi6Ro2elGZf6Rmuc5/rl6nam3P3z.S', true),
-       (3, null, 'aaa@a.pl', 'aaa', '$2a$10$rgTJrdROc5a0MJiTmSU8qewdi6Ro2elGZf6Rmuc5/rl6nam3P3z.S', true),
-       (4, null, 'aaaa@a.pl', 'aaaa', '$2a$10$rgTJrdROc5a0MJiTmSU8qewdi6Ro2elGZf6Rmuc5/rl6nam3P3z.S', true);
-INSERT INTO komornik.categories (id, category_name)
-VALUES (1, 'kategoria'),
-       (2, 'drugaKategoriaaa');
-INSERT INTO komornik.groups (id, default_currency, description, group_name)
-VALUES (1, 'PLN', 'komornik grupa', 'grupa'),
-       (2, 'PLN', null, 'drugaKategoriaaa');
+INSERT INTO komornik.users (id, avatar, mail, username, password, is_verified, created_date)
+VALUES (1, null, 'a@a.pl', 'Janis', '$2a$10$rgTJrdROc5a0MJiTmSU8qewdi6Ro2elGZf6Rmuc5/rl6nam3P3z.S', true, now()),
+       (2, null, 'aa@aa.pl', 'Misiek', '$2a$10$rgTJrdROc5a0MJiTmSU8qewdi6Ro2elGZf6Rmuc5/rl6nam3P3z.S', true, now()),
+       (3, null, 'aaa@a.pl', 'Seba', '$2a$10$rgTJrdROc5a0MJiTmSU8qewdi6Ro2elGZf6Rmuc5/rl6nam3P3z.S', true, now());
+INSERT INTO komornik.categories (id, category_name, created_date)
+VALUES (1, 'general', now());
+INSERT INTO komornik.groups (id, default_currency, description, group_name, created_date)
+VALUES (1, 'PLN', 'dolomity', 'dolomity', now()),
+       (2, 'PLN', null, 'testowa', now());
 INSERT INTO komornik.user_x_group (group_id, user_id)
 VALUES (1, 1),
        (1, 2),
        (1, 3),
-       (1, 4),
        (2, 1),
        (2, 2),
        (2, 3);
